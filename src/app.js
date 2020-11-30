fbf8288fa8ae105112c4a7c530e0b2c0e79e4163
hassanelnajjar <hassansamielnajjar@gmail.com>
2020-11-30 21:13:13 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+require('env2')('./src/configuration/config.env');
+const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+const { join } = require('path');
+const { createWriteStream } = require('fs');
+const router = require('./routes');
+
+const accessFileStream = createWriteStream(
+	join(__dirname, 'logs', 'log-file'),
+	{
+		flags: 'a',
+	}
+);
+const app = express();
+app.use(morgan('combined', { stream: accessFileStream }));
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, '..', 'public'), { maxAge: '30d' }));
+app.set('port', process.env.PORT || 5000);
+
+app.use(router);
+
+module.exports = app;
---
+++ src/tests/unit-testing/pureFunction.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+// just test jest :)
+
+describe('Test all of pure function', () => {
+	test('first test', () => {
+		const expected = 2;
+		const acutal = 2;
+		expect(expected).toBe(acutal);
+	});
+});
