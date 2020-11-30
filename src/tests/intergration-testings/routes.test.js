c4738afec06284a6f8ed52a1faee59ef4215ee9d
hassanelnajjar <hassansamielnajjar@gmail.com>
2020-11-30 21:25:52 +0200

    new commit

--- src/tests/intergration-testings/routes.test.js
+++ src/tests/intergration-testings/routes.test.js
@@ -2,7 +2,18 @@
 // eslint-disable-next-line node/no-unpublished-require
 const request = require('supertest');
 const app = require('../../app');
-console.log('hello');
+
+describe('GET Request', () => {
+	test('GET /', (done) => {
+		request(app).get('/').expect(200, done);
+	});
+});
+describe('GET Request', () => {
+	test('GET /', (done) => {
+		request(app).get('/').expect(200, done);
+	});
+});
+
 describe('GET Request', () => {
 	test('GET /', (done) => {
 		request(app).get('/').expect(200, done);
